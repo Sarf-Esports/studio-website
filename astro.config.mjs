21f9ba956b037a3518c8c95cfbad7d0121752f72
RetoRuto9900K <tutinoko2048@gmail.com>
2025-05-02 02:11:10 +0900

    add: Header, Footer, stylesを整備

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: [
+            '@use "/src/styles/_color.scss" as *;',
+            '@use "/src/styles/_mixin.scss" as *;',
+          ].join('\n'),
+        }
+      }
+    }
+  }
+});
