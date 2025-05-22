71820dd50c16380c3b1a857420bf5965b63d8f74
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-05-23 03:55:20 +0900

    style: フォーマット

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import { sveltePreprocess } from 'svelte-preprocess';
+import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess({ typescript: true }),
+  preprocess: sveltePreprocess({ typescript: true }),
 };
 
 export default config;
