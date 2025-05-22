9f6286d6fa6c976e6faad7d8bc324b1f56092c08
RetoRuto9900K <tutinoko2048@gmail.com>
2025-05-23 02:34:23 +0900

    deps: svelte integrationを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -15,4 +17,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [svelte()],
 });
---
+++ svelte.config.js
@@ -0,0 +1,8 @@
+import { sveltePreprocess } from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: sveltePreprocess({ typescript: true }),
+};
+
+export default config;
