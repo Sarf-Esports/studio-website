b1669b04f06a6815b7ad58ed530516a79d9ed9b1
RetoRuto9900K <tutinoko2048@gmail.com>
2025-05-23 02:57:35 +0900

    feat: 3Dロゴの追加

---
+++ src/components/Logo3d.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Canvas, T } from "@threlte/core";
+	import {
+		ContactShadows,
+		GLTF,
+		Grid,
+		OrbitControls
+	} from "@threlte/extras";
+</script>
+
+<Canvas>
+	<T.PerspectiveCamera makeDefault position={[-10, 90, 25]} fov={3}>
+		<OrbitControls enableZoom enableDamping autoRotate={true} target.y={0.5} />
+	</T.PerspectiveCamera>
+
+	<Grid />
+
+	<!-- <ContactShadows scale={10} blur={2} opacity={0.7} /> -->
+
+	<GLTF
+		url="/models/REVATI-Studio-3D-logo.glb"
+		position={[0, 0.5, 0]}
+		scale={10}
+	/>
+</Canvas>
