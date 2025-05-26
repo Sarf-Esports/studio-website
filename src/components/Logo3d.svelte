e257676921157a1948c17511decf1b46dff3b553
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-05-27 02:13:43 +0900

    fix(DraggableWindow): デザインをそれっぽく変更

--- src/components/Logo3d.svelte
+++ src/components/Logo3d.svelte
@@ -3,25 +3,23 @@
 	import {
 		ContactShadows,
 		GLTF,
-		Grid,
 		OrbitControls
 	} from "@threlte/extras";
 </script>
 
 <div class="logo-container">
 	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[-10, 90, 25]} fov={3}>
-			<OrbitControls enableZoom enableDamping autoRotate={true} target.y={0.5} />
+		<T.PerspectiveCamera makeDefault position={[0, 0, 45]} fov={4.3}>
+			<OrbitControls enableZoom enableDamping autoRotate target.y={0.5} />
 		</T.PerspectiveCamera>
-
-		<Grid />
 
 		<ContactShadows scale={10} blur={2} opacity={0.7} />
 
 		<GLTF
 			url="/models/REVATI-Studio-3D-logo.glb"
-			position={[0, 0.5, 0]}
+			position={[-0.25, 0.5, 0]}
 			scale={10}
+			rotation={[Math.PI * 0.5, 0, 0]}
 		/>
 	</Canvas>
 </div>
