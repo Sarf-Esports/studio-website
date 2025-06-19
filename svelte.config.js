a04322bd04135a8b7b00205dc30dca23b2eaa6f2
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-06-20 00:51:08 +0900

    chore: SvelteでSCSS変数等の自動インポートを追加

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,16 @@
-import { sveltePreprocess } from "svelte-preprocess";
+import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: sveltePreprocess({ typescript: true }),
+  preprocess: sveltePreprocess({
+    typescript: true,
+    scss: {
+      prependData: [
+        '@use "src/styles/_color.scss" as *;',
+        '@use "src/styles/_mixin.scss" as *;',
+      ].join("\n"),
+    },
+  }),
 };
 
 export default config;
