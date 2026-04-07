40e22ebe882c456cf560c5bcb6650bc40bfbced6
RetoRuto9900K <tutinoko2048@gmail.com>
2026-04-08 01:11:19 +0900

    refactor: SSGにrevert, `queryWorks`の返り値にslugを含めるように変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
   vite: {
     css: {
       preprocessorOptions: {
